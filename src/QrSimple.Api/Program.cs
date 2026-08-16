d92bda55bf5b7ab44f1cbb4e2848c1155b69ef14
Tsogt Batjargal <122837521+tsogtbatjargal@users.noreply.github.com>
2026-08-16 23:55:17 +0000

    fix: Update ForwardedHeadersOptions to use KnownIPNetworks and resolve version conflicts in project dependencies

--- src/QrSimple.Api/Program.cs
+++ src/QrSimple.Api/Program.cs
@@ -64,7 +64,7 @@ var forwardedHeadersOptions = new ForwardedHeadersOptions
 {
     ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
 };
-forwardedHeadersOptions.KnownNetworks.Clear();
+forwardedHeadersOptions.KnownIPNetworks.Clear();
 forwardedHeadersOptions.KnownProxies.Clear();
 app.UseForwardedHeaders(forwardedHeadersOptions);
 
